58a2a72a7d9ab97b009bc441e555d99fa4c57e88
WORKSHOP_00 <WORKSHOP_00@unknown>
2016-12-08 12:39:10 -0500

    odataCRUD updated to Fiori 2.0

--- web/resources/odataCRUD/controller/App.controller.js
+++ web/resources/odataCRUD/controller/App.controller.js
@@ -1,19 +1,30 @@
-  sap.ui.controller("opensap.odataBasic.view.App", {
-	onInit: function() {
-		var model = new sap.ui.model.json.JSONModel({});
-		this.getView().setModel(model);
+/*eslint no-console: 0, no-unused-vars: 0, no-use-before-define: 0, no-redeclare: 0, no-undef: 0*/
+//To use a javascript controller its name must end with .controller.js
+sap.ui.define([
+	"opensap/odataBasic/controller/BaseController",
+	"sap/ui/model/json/JSONModel"
+], function(BaseController, JSONModel) {
+	"use strict";
 
-	},
+	return BaseController.extend("opensap.odataBasic.controller.App", {
 
+		onInit: function() {
+			this.getView().addStyleClass("sapUiSizeCompact"); // make everything inside this View appear in Compact mode
+			var oConfig = this.getOwnerComponent().getModel("config");
+			var userName = oConfig.getProperty("/UserName");
+			var userModel = this.getOwnerComponent().getModel("userModel");
+			var oTable = this.getView().byId("userTable");
+			oTable.setModel(userModel);
+		},
+		
 	callUserService: function() {
-		var oModel = sap.ui.getCore().getModel("userModel");
+		var oModel = this.getOwnerComponent().getModel("userModel");
 		var result = this.getView().getModel().getData();
 		var oEntry = {};
 		oEntry.UserId = "0000000000";
 		oEntry.FirstName = result.FirstName;
 		oEntry.LastName = result.LastName;
 		oEntry.Email = result.Email;
-		//oEntry.ZMYNEW1 = "";
 
 		oModel.setHeaders({
 			"content-type": "application/json;charset=utf-8"
@@ -27,7 +38,7 @@
 	},
 
 	callUserUpdate: function() {
-		var oModel = sap.ui.getCore().getModel("userModel");
+		var oModel = this.getOwnerComponent().getModel("userModel");
 		oModel.setHeaders({
 			"content-type": "application/json;charset=utf-8"
 		});
@@ -61,5 +72,6 @@
 			return;
 		}
 
-	}
+	}		
+	});
 });
