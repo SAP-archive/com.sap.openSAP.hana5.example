dc14b0631b12e632ce10c71a4ca637e73e53f81a
WORKSHOP_00 <thomas.jung@sap.com>
2017-01-31 15:34:49 -0500

    Update Multiple Exercise to Fiori 2.0

--- web/resources/multiply/Component.js
+++ web/resources/multiply/Component.js
@@ -1,29 +1,26 @@
-jQuery.sap.declare("sap.shineNext.xsjsMultiply.Component");
+/*eslint no-console: 0, no-unused-vars: 0, no-use-before-define: 0, no-redeclare: 0*/
+sap.ui.define([
+	"sap/ui/core/UIComponent"
+], function(UIComponent) {
+	"use strict";
 
+	return UIComponent.extend("sap.shineNext.xsjsMultiply.Component", {
 
-sap.ui.core.UIComponent.extend("sap.shineNext.xsjsMultiply.Component", {
-	
+	metadata: {
+		manifest: "json"
+	},
+
 	init: function(){
 		jQuery.sap.require("sap.m.MessageBox");
 		jQuery.sap.require("sap.m.MessageToast");
-		sap.ui.core.UIComponent.prototype.init.apply(this, arguments);		
+		sap.ui.core.UIComponent.prototype.init.apply(
+			this, arguments);	
 	},
-	createContent: function() {
-		var oModel = new sap.ui.model.odata.ODataModel("/sap/hana/democontent/epm/services/businessPartners.xsodata/", true);
-      
-		var settings = {
-				ID: "xsjsMultiply",
-				title: "XSJS Multiply Exercise",
-				description: "SHINE XSJS Multiply Exercise"
-			};
-		
-		var oView = sap.ui.view({
-			id: "app",
-			viewName: "sap.shineNext.xsjsMultiply.view.App",
-			type: "XML",
-			viewData: settings
-		});
-
-		return oView;
+	
+	destroy: function() {
+			// call the base component's destroy function
+			UIComponent.prototype.destroy.apply(this, arguments);
 	}
+
+  });
 });
--- web/resources/multiply/view/App.controller.js
+++ web/resources/multiply/view/App.controller.js
@@ -1,57 +1,73 @@
+/*eslint no-console: 0, no-unused-vars: 0, no-use-before-define: 0, no-redeclare: 0, no-undef: 0, no-sequences: 0, no-unused-expressions: 0*/
 //To use a javascript controller its name must end with .controller.js
-sap.ui.controller("sap.shineNext.xsjsMultiply.view.App", {
-    onInit: function() {
-        var model = new sap.ui.model.json.JSONModel({});
-        this.getView().setModel(model);
-        this.getView().addStyleClass("sapUiSizeCompact"); // make everything inside this View appear in Compact mode
-    },
-    onLiveChange: function(oEvent) {
-        var view = this.getView();
-        var result = view.getModel().getData();
-        var controller = this.getView().getController();
-        var valSend;
-        if (oEvent.getParameters().id === "app--val1") {
-            valSend = result.val2;
-        } else {
-            valSend = result.val1;
-        }
-        if (valSend === undefined) {
-            valSend = 0;
-        }
-        var aUrl = "/sap/hana/democontent/epm/services/multiply.xsjs?cmd=multiply" +
-            "&num1=" + escape(oEvent.getParameters().newValue) +
-            "&num2=" + escape(valSend);
-        jQuery.ajax({
-            url: aUrl,
-            method: "GET",
-            dataType: "json",
-            success: controller.onCompleteMultiply,
-            error: controller.onErrorCall
-        });
-    },
+sap.ui.define(["sap/ui/core/mvc/Controller"], function(Controller) {
+	"use strict";
 
-    onCompleteMultiply: function(myTxt) {
-        var oResult = sap.ui.getCore().byId("app--result");
-        if (myTxt === undefined) {
-            oResult.setText(0);
-        } else {
-            jQuery.sap.require("sap.ui.core.format.NumberFormat");
-            var oNumberFormat = sap.ui.core.format.NumberFormat.getIntegerInstance({
-                maxFractionDigits: 12,
-                minFractionDigits: 0,
-                groupingEnabled: true
-            });
-            oResult.setText(oNumberFormat.format(myTxt));
-        }
-    },
+	return Controller.extend("sap.shineNext.xsjsMultiply.view.App", {
 
-    onErrorCall: function(jqXHR) {
-        if (jqXHR.responseText === "NaN") {
-            sap.m.MessageBox.alert("Invalid Input Value");
-        } else {
-            sap.m.MessageBox.alert(jqXHR.responseText);
-        }
-        return;
-    }
+		onInit: function() {
+			var model = new sap.ui.model.json.JSONModel({});
+			this.getView().setModel(model);
+			this.getView().addStyleClass("sapUiSizeCompact"); // make everything inside this View appear in Compact mode
+		},
+		onLiveChange: function(oEvent) {
+			var view = this.getView();
+			var result = view.getModel().getData();
+			var controller = this.getView().getController();
+			var valSend;
+			if (oEvent.getParameters().id === "comp---app--val1") {
+				valSend = result.val2;
+			} else {
+				valSend = result.val1;
+			}
+			if (valSend === undefined) {
+				valSend = 0;
+			}
+			var aUrl = "/sap/hana/democontent/epm/services/multiply.xsjs?cmd=multiply" +
+				"&num1=" + escape(oEvent.getParameters().newValue) +
+				"&num2=" + escape(valSend);
+			jQuery.ajax({
+				url: aUrl,
+				method: "GET",
+				dataType: "json",
+				success: controller.onCompleteMultiply,
+				error: controller.onErrorCall
+			});
+		},
 
+		onCompleteMultiply: function(myTxt) {
+			var oResult = sap.ui.getCore().byId("comp---app--result");
+			if (myTxt === undefined) {
+				oResult.setText(0);
+			} else {
+				jQuery.sap.require("sap.ui.core.format.NumberFormat");
+				var oNumberFormat = sap.ui.core.format.NumberFormat.getIntegerInstance({
+					maxFractionDigits: 12,
+					minFractionDigits: 0,
+					groupingEnabled: true
+				});
+				oResult.setText(oNumberFormat.format(myTxt));
+			}
+		},
+
+		onErrorCall: function(oError) {
+			if (oError.statusCode === 500 || oError.statusCode === 400 || oError.statusCode === "500" || oError.statusCode === "400") {
+				var errorRes = JSON.parse(oError.responseText);
+				if (!errorRes.error.innererror) {
+					sap.m.MessageBox.alert(errorRes.error.message.value);
+				} else {
+					if (!errorRes.error.innererror.message) {
+						sap.m.MessageBox.alert(errorRes.error.innererror.toString());
+					} else {
+						sap.m.MessageBox.alert(errorRes.error.innererror.message);
+					}
+				}
+				return;
+			} else {
+				sap.m.MessageBox.alert(oError.response.statusText);
+				return;
+			}
+
+		}
+	});
 });
