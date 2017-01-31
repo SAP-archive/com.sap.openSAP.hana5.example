3dca1e2f0bc22c107bd789b577e938b9df9a5bcb
WORKSHOP_00 <thomas.jung@sap.com>
2017-01-31 13:44:18 -0500

    exerciseAsync Update to Fiori 2.0

--- web/resources/exerciseAsync/Component.js
+++ web/resources/exerciseAsync/Component.js
@@ -1,36 +1,53 @@
-jQuery.sap.declare("sap.xs.Exercise3.Component");
+/*eslint no-console: 0, no-unused-vars: 0, no-use-before-define: 0, no-redeclare: 0*/
+sap.ui.define([
+	"sap/ui/core/UIComponent"
+], function(UIComponent) {
+	"use strict";
 
+	return UIComponent.extend("sap.xs.exerciseAsync.Component", {
 
-sap.ui.core.UIComponent.extend("sap.xs.Exercise3.Component", {
+	metadata: {
+		manifest: "json"
+	},
+
 	init: function(){
 		jQuery.sap.require("sap.m.MessageBox");
-		jQuery.sap.require("sap.m.MessageToast");		
+		jQuery.sap.require("sap.m.MessageToast");
+		
+		sap.ui.core.UIComponent.prototype.init.apply(
+			this, arguments);
+		this.getSessionInfo();
+		
 		// Chat Model
-      	var oModel = new sap.ui.model.json.JSONModel();
+      	var oModel = this.getModel("chatModel");
        	oModel.setData({      		
         	chat: "",
         	message: ""
       	});
-      	sap.ui.getCore().setModel(oModel,"chatModel");
-         
-		sap.ui.core.UIComponent.prototype.init.apply(this, arguments);
 	},
 	
-	createContent: function() {
-     
-		var settings = {
-				ID: "chatRoot",
-				title: "Exercise 3 Asynch Test Framework",
-				description: "Exercise 3 Asynch Test Framework"
-			};
-		
-		var oView = sap.ui.view({
-			id: "app",
-			viewName: "sap.xs.Exercise3.view.App",
-			type: "XML",
-			viewData: settings
-		});
-		oView.setModel(sap.ui.getCore().getModel("chatModel"));
-    	return oView;
-	}
+	destroy: function() {
+			// call the base component's destroy function
+			UIComponent.prototype.destroy.apply(this, arguments);
+	},
+
+	getSessionInfo: function() {
+			var aUrl = "/xsjs/exercisesMaster.xsjs?cmd=getSessionInfo";
+			this.onLoadSession(
+				JSON.parse(jQuery.ajax({
+					url: aUrl,
+					method: "GET",
+					dataType: "json",
+					async: false
+				}).responseText));
+	},
+
+	onLoadSession: function(myJSON) {
+			for (var i = 0; i < myJSON.session.length; i++) {
+				var config = this.getModel("config");
+				config.setProperty("/UserName", myJSON.session[i].UserName);
+			}
+		}
+	});
+
 });
