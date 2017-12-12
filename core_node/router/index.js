7ba0fe65abb4106a6e3cc45d90e52033e3ee9dc1
WORKSHOP_00 <thomas.jung@sap.com>
2017-12-12 14:54:57 -0500

    Clean up of refactoring of node.js and XSJS to separate modules

---
+++ core_node/router/index.js
@@ -0,0 +1,20 @@
+/*eslint-env node, es6 */
+"use strict";
+
+module.exports = (app, server) => {
+	app.use("/node", require("./routes/myNode")());
+	app.use("/node/excAsync", require("./routes/exerciseAsync")(server));
+	app.use("/node/textBundle", require("./routes/textBundle")());
+	app.use("/node/chat", require("./routes/chatServer")(server));
+	app.use("/node/excel", require("./routes/excel")());
+	app.use("/node/xml", require("./routes/xml")());
+	app.use("/node/zip", require("./routes/zip")());
+	app.use("/node/cds", require("./routes/cds")());
+	app.use("/node/auditLog", require("./routes/auditLog")());
+
+	app.use("/sap/bc/lrep", require("./routes/lrep")());
+	app.use("/node/annotations", require("./routes/annotations")());
+	app.use("/node/JavaScriptBasics", require("./routes/JavaScriptBasics")());
+	app.use("/node/promises", require("./routes/promises")());
+	app.use("/node/es6", require("./routes/es6")());
+};
