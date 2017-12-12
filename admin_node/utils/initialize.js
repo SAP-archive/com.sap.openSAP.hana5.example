57be0294da023524d7de252c5afeda2aeb3f391b
WORKSHOP_00 <thomas.jung@sap.com>
2017-12-12 13:42:23 -0500

    Move Jobs tables to Core_DB and then reorg all Job Scheduling and Data Generator functionality into a new Node.js module called admin_node

---
+++ admin_node/utils/initialize.js
@@ -0,0 +1,80 @@
+/*eslint no-console: 0, no-unused-vars: 0*/
+"use strict";
+module.exports = {
+	initExpress: function() {
+		var xsenv = require("@sap/xsenv");
+		var passport = require("passport");
+		var xssec = require("@sap/xssec");
+		var xsHDBConn = require("@sap/hdbext");
+		var express = require("express");
+
+		//logging
+		var logging = require("@sap/logging");
+		var appContext = logging.createAppContext();
+
+		//Initialize Express App for XS UAA and HDBEXT Middleware
+		var app = express();
+		passport.use("JWT", new xssec.JWTStrategy(xsenv.getServices({
+			uaa: {
+				tag: "xsuaa"
+			}
+		}).uaa));
+		app.use(logging.expressMiddleware(appContext));
+		app.use(passport.initialize());
+
+		var hanaOptions = xsenv.getServices({
+			hana: {
+				tag: "hana"
+			}
+		});
+		hanaOptions.hana.rowsWithMetadata = true;
+		app.use(
+			passport.authenticate("JWT", {
+				session: false
+			}),
+			xsHDBConn.middleware(hanaOptions.hana)
+		);
+		return app;
+	},
+
+	initXSJS: function(app) {
+		var xsjs = require("@sap/xsjs");
+		var xsenv = require("@sap/xsenv");
+		var options = {
+			//	anonymous : true, // remove to authenticate calls
+			redirectUrl: "/index.xsjs"
+		};
+
+		// configure HANA
+		try {
+			options = Object.assign(options, xsenv.getServices({
+				hana: {
+					tag: "hana"
+				}
+			}));
+
+			options = Object.assign(options, xsenv.getServices({
+				secureStore: {
+					tag: "hana"
+				}
+			}));
+		} catch (err) {
+			console.log("[WARN]", err.message);
+		}
+
+		// configure UAA
+		try {
+			options = Object.assign(options, xsenv.getServices({
+				uaa: {
+					tag: "xsuaa"
+				}
+			}));
+		} catch (err) {
+			console.log("[WARN]", err.message);
+		}
+
+		// start server
+		var xsjsApp = xsjs(options);
+		app.use(xsjsApp);
+	}
+};
