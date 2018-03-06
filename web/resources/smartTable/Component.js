52af13f4132516b9e189bb6abafa729dfa885705
I809764 <thomas.jung@sap.com>
2018-03-06 13:49:00 -0500

    smartTable annotation update

--- web/resources/smartTable/Component.js
+++ web/resources/smartTable/Component.js
@@ -1,7 +1,9 @@
 jQuery.sap.declare("sap.openSAP.smarttable.Component");
+sap.ui.getCore().loadLibrary("sap.ui.generic.app");
+jQuery.sap.require("sap.ui.generic.app.AppComponent");
 
 sap.ui.core.UIComponent.extend("sap.openSAP.smarttable.Component", {
-
+//  sap.ui.generic.app.AppComponent.extend("sap.openSAP.smarttable.Component", { 
 	metadata: {
 		manifest: "json",
 
