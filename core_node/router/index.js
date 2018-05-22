52e94d33ac925e67d4ea3bc0334391dfa15486de
I809764 <thomas.jung@sap.com>
2018-05-22 12:56:41 -0400

    Add @sap/hana-client example

--- core_node/router/index.js
+++ core_node/router/index.js
@@ -3,6 +3,7 @@
 
 module.exports = (app, server) => {
 	app.use("/node", require("./routes/myNode")());
+	app.use("/node/client", require("./routes/hanaClient")());
 	app.use("/node/excAsync", require("./routes/exerciseAsync")(server));
 	app.use("/node/textBundle", require("./routes/textBundle")());
 	app.use("/node/chat", require("./routes/chatServer")(server));
