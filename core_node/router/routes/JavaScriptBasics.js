2513228a2f250f6bf9905a3cdb6002fff4bd7180
Thomas Jung <thomas.jung@sap.com>
2018-06-19 15:23:37 -0400

    String Literal Updates

--- core_node/router/routes/JavaScriptBasics.js
+++ core_node/router/routes/JavaScriptBasics.js
@@ -25,22 +25,25 @@ module.exports = function() {
 		var now = new Date();
 		var nextMonth = new Date();
 
-		body += "Now: " + now + "</p>";
-		body += "Now UTC: " + now.toUTCString() + "</p>";
-		body += "Now Date String: " + now.toDateString() + "</p>";
-		body += "Now Locale Date String: " + now.toLocaleDateString() + "</p>";
-		body += "Now Locale Time String: " + now.toLocaleTimeString() + "</p>";
-		body += "Now Locale String: " + now.toLocaleString() + "</p>";
-		body += "Now ISO String: " + now.toISOString() + "</p>";
-		body += "Now JSON String: " + now.toJSON() + "</p>";
-		body += "Now Year: " + now.getFullYear() + "</p>";
-		body += "Now Month: " + now.getMonth() + "</p>";
-		body += "Now Day of Week: " + now.getDay() + "</p>";
-		body += "Now Day of Month: " + now.getDate() + "</p>";
-		body += "Now number of milliseconds since midnight Jan 1, 1970: " + now.getTime() + "</p>";
-		body += "Now Hours: " + now.getHours() + "</p>";
-		body += "Now Minutes: " + now.getMinutes() + "</p>";
-		body += "Now Seconds: " + now.getSeconds() + "</p>";
+	body += `Now: ${now} </p>`;
+	body += `Now UTC: ${now.toUTCString()} </p>`;
+	body += `Now Date String: ${now.toDateString()} </p>`;	
+	body += `Now Locale Date String: ${now.toLocaleDateString()} </p>`;	
+	body += `Now Locale Time String: ${now.toLocaleTimeString()} </p>`;	
+	body += `Now Locale String: ${now.toLocaleString()} </p>`;		
+	body += `Now ISO String: ${now.toISOString()} </p>`;	
+	body += `Now JSON String: ${now.toJSON()} </p>`;	
+	body += `Now Year: ${now.getFullYear()} </p>`;
+	body += `Now Month: ${now.getMonth()} </p>`;	
+	body += `Now Day of Week: ${now.getDay()} </p>`;
+	body += `Now Day of Month: ${now.getDate()} </p>`;
+	body += `Now number of milliseconds since midnight Jan 1, 1970: ${now.getTime()} </p>`;	
+	body += `Now Hours: ${now.getHours()} </p>`;
+	body += `Now Minutes: ${now.getMinutes()} </p>`;
+	body += `Now Seconds: ${now.getSeconds()} </p>`;	
+	
+	nextMonth.setDate(now.getDate()+30);
+	body += `30 days from now: ${nextMonth} </p>`;
 
 		nextMonth.setDate(now.getDate() + 30);
 		body += "30 days from now: " + nextMonth + "</p>";
@@ -242,24 +245,24 @@ module.exports = function() {
 		var body = "";
 		var demo1 = "SAP HANA Extended Application Services";
 
-		body += "Basic String: " + demo1 + "</p>";
+			body += `Basic String: ${demo1} </p>`;
 
-		body += "The first character in the string: " + demo1[0] + "</p>";
+	body += `The first character in the string: ${demo1[0]} </p>`;
 
-		body += "The length of the string: " + demo1.length.toString() + "</p>";
+	body += `The length of the string: ${demo1.length.toString()} </p>`;
 
-		//slice with a negative index
-		body += "The last character in the string: " + demo1.slice(-1) + "</p>";
+	//slice with a negative index
+	body += `The last character in the string: ${demo1.slice(-1)} </p>`;
 
-		body += "Upper: " + demo1.toUpperCase() + "</p>";
+	body += `Upper: ${demo1.toUpperCase()} </p>`;
 
-		body += "Lower: " + demo1.toLowerCase() + "</p>";
+	body += `Lower: ${demo1.toLowerCase()} </p>`;
 
-		body += "Find HANA: " + demo1.indexOf("HANA").toString() + "</p>";
+	body += `Find HANA: ${demo1.indexOf("HANA").toString()} </p>`;
 
-		body += "Find Last occurance of the letter A: " + demo1.lastIndexOf("A").toString() + "</p>";
+	body += `Find Last occurance of the letter A: ${demo1.lastIndexOf("A").toString()} </p>`;
 
-		body += "Replace with XS: " + demo1.replace("Extended Application Services", "XS") + "</p>";
+	body += `Replace with XS: ${demo1.replace("Extended Application Services", "XS")} </p>`;
 
 		var es6 = "ES6!";
 		body += `Template Literals in ${es6}` + "</p>";
